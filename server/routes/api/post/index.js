a24cc6cc9b6039ed1319270a15220d20bee86324
Yakubov01 <lord17cl@gmail.com>
2021-10-12 00:37:16 +0500

    The post direction is about 35% ready

---
+++ server/routes/api/post/index.js
@@ -0,0 +1,92 @@
+const { Post } = require("../../../models/post");
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
+
+// UPLOAD IMAGES WITH MULTER
+
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+// init upload
+const upload = multer({
+  storage: storage,
+}).single("images");
+
+router.post("/upload_images", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.status(400).json({
+        result: "failed",
+        message: `Cannot upload files. Error is ${err}`,
+      });
+    } else {
+      if (req.file === undefined) {
+        res.status(400).json({
+          result: "failed",
+          message: "You are not submit images",
+        });
+      } else {
+        console.log(req.file);
+        res.status(200).json({
+          result: "ok",
+          message: "Upload image successfully",
+          path: req.file.originalname,
+        });
+      }
+    }
+  });
+});
+
+// route api/posts
+// CREATE A NEW POST
+// access PUBLIC
+
+router.post("/posts", (req, res) => {
+  const { title, author, description, category, images } = req.body;
+
+  const newPost = new Post({
+    title,
+    author,
+    description,
+    category,
+    images,
+  });
+
+  newPost
+    .save()
+    .then((post) => {
+      res.status(200).json(post);
+      // res.redirect('/admin/posts')
+    })
+    .catch((err) => res.status(400).json(err));
+});
+
+// route api/posts
+// GET ALL POSTS
+// access PUBLIC
+
+router.get("/posts", (req, res) => {
+  Post.find()
+    .sort({ createOn: -1 })
+    .then((post) => res.status(200).json(post))
+    .catch((err) => res.status(400).json(err));
+});
+
+// route api/posts/:id
+// DELETE A POST
+// access PUBLIC
+
+router.delete("/posts/:id", (req, res) => {
+  const id = req.params.id;
+
+  Post.findByIdAndDelete(id)
+    .then((post) => res.status(200).json(post))
+    .catch((err) => res.status(400).json(err));
+});
+
+module.exports = router;
