0f15aefae7c48b105e3cff71c8cab1a95efc13bb
Yakubov01 <lord17cl@gmail.com>
2021-10-11 20:06:40 +0500

    use post route

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/", require("./routes/api/post"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
