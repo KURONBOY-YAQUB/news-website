d47e625e41aec655f6d3b6d14b52b9c181897294
Yakubov01 <lord17cl@gmail.com>
2021-10-31 18:02:02 +0500

    change model comment

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,32 +2,30 @@ const mongoose = require("mongoose");
 const { PostSchema } = require("../models/post");
 const { UserSchema } = require("../models/user");
 
-const CommentSchema = new mongoose.Schema({
-  commentBody: {
-    type: String,
-    required: true,
+const CommentSchema = new mongoose.Schema(
+  {
+    commentBody: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    postId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Post",
+      required: true,
+    },
   },
-  commentDate: {
-    type: Date,
-    default: Date.now,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    // required: true,
-  },
-  //Tipini ko'rsatibsan, post jadvali bilan bog'lanish qani?
-  //Hamma modellaringda shu xatolik tyog'rla!
-  //Qolgan polyalarga ham REF qo'sh va aniqlashtirib ishla
-  postId: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Post",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Comment = mongoose.model("Comment", CommentSchema);
+const Comments = mongoose.model("Comments", CommentSchema);
 module.exports = {
-  Comment,
+  Comments,
   CommentSchema,
 };
