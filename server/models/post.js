963840446bf5943d9f8e14513f9988daa4bae94b
Yakubov01 <lord17cl@gmail.com>
2021-10-31 18:08:28 +0500

    change model post

--- server/models/post.js
+++ server/models/post.js
@@ -1,38 +1,42 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    images: {
+      type: String,
+      // required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    tags: {
+      type: [String],
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  createOn: {
-    type: Date,
-    default: Date.now,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  images: {
-    type: String,
-    // required: true,
-  },
-  slug: {
-    type: String,
-    required: true,
-  },
-  tags: {
-    type: [String],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.model("Post", PostSchema);
 
