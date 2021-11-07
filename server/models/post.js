b8743512c3ed7e99be8283f798140b0e79f9dd09
Yakubov01 <lord17cl@gmail.com>
2021-11-07 22:46:09 +0500

    errors have been corrected

--- server/models/post.js
+++ server/models/post.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
 const PostSchema = new mongoose.Schema(
   {
     title: {
@@ -38,43 +37,6 @@ const PostSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-=======
-const PostSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: Schema.Types.ObjectId, ref: "User",
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    
-    type: Schema.Types.ObjectId, ref: "Category",
-    required: true,
-  },
-  images: {
-    type: String,
-    // required: true,
-  },
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  tags: {
-    type: [String],
-  },
-},
-                                       {
-        timestamps: true,
-    }
-                      );
->>>>>>> 83e603d7e7655629398d6824dd039eae70c1d9de
 
 const Post = mongoose.model("Post", PostSchema);
 
